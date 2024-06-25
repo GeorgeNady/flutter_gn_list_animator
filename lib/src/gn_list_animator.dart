236a014a4c76576663b9ed8b01043129ad45ba5c
GeorgeNady <gorgonad.gn@gmail.com>
2024-06-25 21:09:23 +0300

    adding scroll direction adding separator

--- lib/src/gn_list_animator.dart
+++ lib/src/gn_list_animator.dart
@@ -3,21 +3,25 @@ import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class GnListAnimator extends StatefulWidget {
   final List<Widget> children;
+  final Widget? separator;
   final int? durationInMillis;
   final double? verticalOffset;
   final double? horizontalOffset;
   final bool shrinkWrap;
   final EdgeInsetsGeometry? padding;
+  final Axis scrollDirection;
   final Curve? curve;
 
   const GnListAnimator({
     super.key,
     required this.children,
+    this.separator,
     this.durationInMillis,
     this.verticalOffset,
     this.horizontalOffset,
     this.shrinkWrap = true,
     this.padding,
+    this.scrollDirection = Axis.vertical,
     this.curve,
   });
 
@@ -29,12 +33,13 @@ class _GnListAnimatorState extends State<GnListAnimator> {
   @override
   Widget build(BuildContext context) {
     return AnimationLimiter(
-      child: ListView.builder(
+      child: ListView.separated(
         clipBehavior: Clip.none,
         padding: widget.padding,
         physics: const ScrollPhysics(),
         shrinkWrap: widget.shrinkWrap,
         itemCount: widget.children.length,
+        scrollDirection: widget.scrollDirection,
         itemBuilder: (_, index) => AnimationConfiguration.staggeredList(
           position: index,
           delay: Duration(milliseconds: widget.durationInMillis ?? 100),
@@ -47,6 +52,7 @@ class _GnListAnimatorState extends State<GnListAnimator> {
             ),
           ),
         ),
+        separatorBuilder: (_, index) => widget.separator ?? const SizedBox(),
       ),
     );
   }
